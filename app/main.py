401fa5df32dbb4a496d6efc0449d4b5e87d1cf4b
JackBaker <AlMcD@yandex.com>
2019-02-13 21:30:32 +0100

    length - 1

--- app/main.py
+++ app/main.py
@@ -60,14 +60,14 @@ def move():
     me = data['you']['body']
     health = data['you']['health']
     length = len(me)
-    #m = 2
+    #m = 1
     HeadX = me[0]['x']
     HeadY = me[0]['y']
     TailX = me[length-1]['x']
     TailY = me[length-1]['y']
     #if health == 100:
-    #    m = 1
-    for i in range(length):
+    #    m = 0
+    for i in range(length - 1):
         a = [[me[i]['x'],me[i]['y']]]
         walls.extend(a)
 
