f832c36d2f7740db6dc7f63c6ab085f0d530d820
jbaker96 <AlMcD@yandex.com>
2020-01-12 12:14:47 -0800

    testing1212

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from api import ping_response, start_response, move_response, end_response
 @bottle.route('/')
 def index():
     return '''
-    Testing Shit out.
+    Testing Shit out for real this time.
     '''
 
 @bottle.route('/static/<path:path>')
