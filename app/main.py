b7056b48a7533d576d1a3e2d5ec276800b24ba9c
jbaker96 <AlMcD@yandex.com>
2020-01-12 20:04:36 -0800

    gold

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def ping():
 
 @bottle.post('/start')
 def start():
-    startinput = {  "color": "#FFC100", 
+    startinput = {  "color": "gold", 
                     #"headType": "tongue", 
                     #"tailType": "curled"
                 }
