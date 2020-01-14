7a4e2e130ae0c08107989766eec01b06b5545e62
jbaker96 <AlMcD@yandex.com>
2020-01-13 17:45:08 -0800

    Gold Hex

--- app/main.py
+++ app/main.py
@@ -21,14 +21,14 @@ def ping():
 
 @bottle.post('/start')
 def start():
-    startinput = { "color": "gold" 
+    startinput =    { "color": "#FFC100" 
                     #"headType": "tongue", 
                     #"tailType": "curled"
-                }
-    jsonoutput = json.dumps(startinput)
+                    }
+    #jsonoutput = json.dumps(startinput)
     #headType = "tongue"
     #color = "#FFC100"
-    return start_response(jsonoutput)
+    return start_response(startinput)
 
 @bottle.post('/move')
 def move():
