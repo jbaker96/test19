38b512f1ff9922f111dab29a7c244f3fb72f6a2c
jbaker96 <AlMcD@yandex.com>
2020-01-23 20:15:08 -0800

    Fied Indents

--- app/main.py
+++ app/main.py
@@ -325,14 +325,14 @@ def move():
         FoodX = FoodList[counter]['x']
         FoodY = FoodList[counter]['y']
 
-        GoalX = FoodX - HeadX
-        GoalY = FoodY - HeadY
+        #GoalX = FoodX - HeadX
+        #GoalY = FoodY - HeadY
         goal = [FoodX, FoodY]
         #Check Left
         Left = 0
         if FindTail([HeadX - 1, HeadY], walls, checked, goal, count) == True:
-            #if FindTail(goal, walls, checked, tail, count) == True:
-            Left = count[0]
+            if FindTail(goal, walls, checked, tail, count) == True:
+                Left = count[0]
                 '''if [HeadX - 1, HeadY] in danger:
                     Left = Left + 100'''
         #Reset
@@ -341,8 +341,8 @@ def move():
         #Check Right
         Right = 0
         if FindTail([HeadX + 1, HeadY], walls, checked, goal, count) == True:
-            #if FindTail(goal, walls, checked, tail, count) == True:
-            Right = count[0]
+            if FindTail(goal, walls, checked, tail, count) == True:
+                Right = count[0]
                 '''if [HeadX + 1, HeadY] in danger:
                     Right = Right + 100'''
         #Reset
@@ -351,8 +351,8 @@ def move():
         #Check Up
         Up = 0
         if FindTail([HeadX, HeadY - 1], walls, checked, goal, count) == True:
-            #if FindTail(goal, walls, checked, tail, count) == True:
-            Up = count[0]
+            if FindTail(goal, walls, checked, tail, count) == True:
+                Up = count[0]
                 '''if [HeadX, HeadY - 1] in danger:
                     Up = Up + 100'''
         #Reset
@@ -361,8 +361,8 @@ def move():
         #Check Down
         Down = 0
         if FindTail([HeadX, HeadY + 1], walls, checked, goal, count) == True:
-            #if FindTail(goal, walls, checked, tail, count) == True:
-            Down = count[0]
+            if FindTail(goal, walls, checked, tail, count) == True:
+                Down = count[0]
                 '''if [HeadX, HeadY + 1] in danger:
                     Down = Down + 100'''
         #Reset
